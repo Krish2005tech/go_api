597753c1faddcad0a1674072f69dd34aaa6de578
Krish Teckchandani <krish2005tech@gmail.com>
2025-06-12 16:46:10 +0530

    logging and cors added

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Krish2005tech/go_api
 
 go 1.24.3
+
+require github.com/rs/cors v1.11.1 // indirect
--- server.go
+++ server.go
@@ -3,8 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
-	
+
+	"log/slog"
+	"os"
+
+	"github.com/rs/cors"
 )
 
 type CalcRequest struct {
@@ -25,6 +30,7 @@ type CalcResponse struct {
 func calculateHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, "Only GET method allowed", http.StatusMethodNotAllowed)
+		logger.Error("Invalid method", "method", r.Method, "path", r.URL.Path)
 		return
 	}
 
@@ -32,16 +38,19 @@ func calculateHandler(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
 		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		logger.Error("Failed to decode JSON", "error", err, "path", r.URL.Path)
 		return
 	}
 
 	// 👉 TODO: Implement operation logic here
 	if req.Op == "" {
 		http.Error(w, "Operation not specified", http.StatusBadRequest)
+		logger.Error("operation not specified", "path", r.URL.Path)
 		return
 	}
 	if req.Op != "add" && req.Op != "subtract" && req.Op != "multiply" && req.Op != "divide" {
 		http.Error(w, "Invalid operation", http.StatusBadRequest)
+		logger.Error("invalid operator", "op", req.Op)
 		return
 	}
 	var output int
@@ -55,11 +64,13 @@ func calculateHandler(w http.ResponseWriter, r *http.Request) {
 		case "divide":
 			if req.B == 0 {
 				http.Error(w, "Division by zero", http.StatusBadRequest)
+				logger.Error("division by zero", "a", req.A, "b", req.B)
 				return
 			}
 			output = req.A / req.B
 		default:
 			http.Error(w, "Unknown operation", http.StatusBadRequest)
+			logger.Error("invalid operator", "op", req.Op)
 			return
 	}
 
@@ -146,7 +157,37 @@ func divideHandler(w http.ResponseWriter, r *http.Request) {
 
 
 
+var logger *slog.Logger
 
+func init() {
+	file, err := os.OpenFile("server.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+if err != nil {
+	log.Fatal(err)
+}
+
+logger = slog.New(slog.NewTextHandler(file, &slog.HandlerOptions{Level: slog.LevelInfo}))
+
+}
+
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ip := r.RemoteAddr
+		if realIP := r.Header.Get("X-Real-IP"); realIP != "" {
+			ip = realIP
+		} else if fwdFor := r.Header.Get("X-Forwarded-For"); fwdFor != "" {
+			ip = fwdFor
+		}
+
+		logger.Info("received request",
+			"method", r.Method,
+			"path", r.URL.Path,
+			"ip", ip,
+		)
+
+		next.ServeHTTP(w, r)
+	})
+}
 
 
 func main() {
@@ -154,13 +195,20 @@ func main() {
 	// fmt.Println("Server started at http://localhost:8080")
 	// http.ListenAndServe(":8080", nil)
 	server := http.NewServeMux()
+
 	server.HandleFunc("/calculate", calculateHandler)
 	server.HandleFunc("/add",addHandler)
 	server.HandleFunc("/subtract", subtractHandler)
 	server.HandleFunc("/multiply", multiplyHandler)
 	server.HandleFunc("/divide", divideHandler)
+
+	corshandler := cors.AllowAll().Handler(server)
+	logHandler := loggingMiddleware(corshandler)
+	
 	fmt.Println("Server started at http://localhost:8080")
-	http.ListenAndServe(":8080", server)
+	logger.Info("Server starting on :8080")
+
+	http.ListenAndServe(":8080", logHandler)
 
 	//test curl
 	// curl -X GET http://localhost:8080/calculate -H "Content-Type: application/json" -d "{\"a\":10, \"b\":4, \"op\":\"add\"}"
